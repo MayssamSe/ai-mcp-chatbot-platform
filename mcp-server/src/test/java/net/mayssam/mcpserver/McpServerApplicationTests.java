00b02db569f616bd3eb878ee0e6899faa5195ab6
Mayssam Sehait <sehaitmayssam@gmail.com>
2025-07-11 01:28:14 +0200

    Initial commit: projet complet avec serveur Spring, client Python et Node

---
+++ mcp-server/src/test/java/net/mayssam/mcpserver/McpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mayssam.mcpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
